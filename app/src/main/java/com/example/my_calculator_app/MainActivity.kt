1f828c2b4cbedb861cf4aa7e7613d5bf1a9518f4
SherryPham <phamthuthu204@gmail.com>
2025-01-16 16:38:14 +1100

    import view

--- app/src/main/java/com/example/my_calculator_app/MainActivity.kt
+++ app/src/main/java/com/example/my_calculator_app/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.my_calculator_app
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var btnAdd : Button
     lateinit var btnSub : Button
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnDiv : Button
     lateinit var a : EditText
     lateinit var b : EditText
+    lateinit var  result : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
